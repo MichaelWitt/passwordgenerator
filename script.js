0c6d361edacf4cdd87365dbc943ced9705b137de
Annema Cozmoz <mikewittorp@gmail.com>
2020-09-17 18:06:43 -0400

    created a new function and editing logic

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var characters = prompt('How many characters would you like in your password?');
-var special = confirm('Click OK if you would like special characters in your password.');
-var numbers = confirm('Click OK if you would like numbers in your password.');
-var lowercase = confirm('Click OK if you would like lowercase characters in your password.');
-var upperCase = confirm('Click OK if you would like uppercase characters in your password.');
 
-var specialSymbols = ["!", "#", "$", "%", "&", "'", "()", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "`", "{ ", "|", "}", "~"];
+var specialSymbols = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "`", "{ ", "|", "}", "~"];
 
 var numberSymbols = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
@@ -15,49 +10,59 @@ var lowerSymbols = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
 
 var upperSymbols = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+function generatePassword() {
+  var characters = prompt('How many characters would you like in your password?');
+  var special = confirm('Click OK if you would like special characters in your password.');
+  var numbers = confirm('Click OK if you would like numbers in your password.');
+  var lowercase = confirm('Click OK if you would like lowercase characters in your password.');
+  var uppercase = confirm('Click OK if you would like uppercase characters in your password.');
+  var passwordCharacters = [];
 
 
-for (let i = 0; i < password.length; i++) {
-
-
-
-  if (special === true) {
-    password.push(specialSymbols)
+  if (special) {
+    passwordCharacters = passwordCharacters.concat(specialSymbols)
   };
 
-  if (numbers === true) {
-    password.push(numberSymbols)
+  if (numbers) {
+    passwordCharacters = passwordCharacters.concat(numberSymbols)
   };
 
-  if (lowercase === true) {
-    password.push(lowerSymbols)
+  if (lowercase) {
+    passwordCharacters = passwordCharacters.concat(lowerSymbols)
   };
 
-  if (uppercase === true) {
-    password.push(upperSymbols)
+  if (uppercase) {
+    passwordCharacters = passwordCharacters.concat(upperSymbols)
   };
 
+  console.log(passwordCharacters);
 
-  // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
-
+  for (let i = 0; i < password.length; i++) {
   }
 
-
-
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+}
 
 
 
 
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
+  passwordText.value = password;
 
 }
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
 
 
 
