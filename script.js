d22b48a5fe29b843303eb723dc7ad8f1b4839607
Annema Cozmoz <mikewittorp@gmail.com>
2020-09-17 14:00:32 -0400

    added variables with alerts

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var characters = prompt('How many characters would you like in your password?');
+var specialCharacters = confirm('Click OK if you would like special characters in your password.');
+var numbers = confirm('Click OK if you would like numbers in your password.');
+var lowerCase = confirm('Click OK if you would like lowercase characters in your password.');
+var upperCase = confirm('Click OK if you would like uppercase characters in your password.');
 
 // Write password to the #password input
 function writePassword() {
@@ -9,6 +14,13 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+// if (characters === true) {
+
+
+// }
+
+// " !"#$ %& '()*+,-./:;<=>?@[\]^_`{|}~"
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
