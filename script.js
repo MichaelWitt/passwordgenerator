9a1addb3a84a1031d6173a6f6293895f2c091278
Annema Cozmoz <mikewittorp@gmail.com>
2020-09-20 13:29:52 -0400

    Updated javascript with details on what each function, for loop, variable, and if statements do, and how they work.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-// Assignment Code
+// Password Generator JS
+
+// Linking JS to HTML 
 var generateBtn = document.querySelector("#generate");
 
-
+// Variables to generate password characters from
 var specialSymbols = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "`", "{", "|", "}", "~", "]"];
 
 var numberSymbols = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -10,30 +12,31 @@ var lowerSymbols = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
 
 var upperSymbols = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+// Function to collect the desired password length of 8 to 128 characters
 function generatePassword() {
   var characters = prompt('How many characters would you like in your password?');
 
   if (characters < 8 || characters > 128) {
     alert('Password length must be between 8 and 128 characters');
     generatePassword();
-
-
   }
 
+  // Variables & alerts to collect user's desired character password preference
   var special = confirm('Click OK if you would like special characters in your password.');
   var numbers = confirm('Click OK if you would like numbers in your password.');
   var lowercase = confirm('Click OK if you would like lowercase characters in your password.');
   var uppercase = confirm('Click OK if you would like uppercase characters in your password.');
 
+  // If statement to make it so user has to choose at least one character type
   if (!special && !numbers && !lowercase && !uppercase) {
     alert('Please confirm at least one character type.');
     generatePassword();
   }
 
-
+  // Created a blank array to feed the selected characters into
   var passwordCharacters = [];
 
-
+  // If statements that add selected character type arrays and concats them into the passwordCharacters array
   if (special) {
     passwordCharacters = passwordCharacters.concat(specialSymbols)
   };
@@ -50,34 +53,28 @@ function generatePassword() {
     passwordCharacters = passwordCharacters.concat(upperSymbols)
   };
 
-
+  // Created a variable that the random characters from the passwordCharacters array will feed into
   var generatedPassword = ""
 
   for (let i = 0; i < characters; i++) {
-    //generate random number between 0 and array length minus 1
+    // Generates random number between 0 and array length minus 1
     var randomNum = Math.floor(Math.random() * passwordCharacters.length);
-    //select it from PasswordCharacters and concat to generatedPassword
+    // Selects random password from the PasswordCharacters array and concats to generatedPassword
     generatedPassword += passwordCharacters[randomNum];
-
-
   }
-  //remove first four elements of the array and make sure that all arrays are in password. 
+  // Removes the first four elements of the array and makes sure that all arrays are in the password
   return generatedPassword;
 }
 
-
-
-
-// Write password to the #password input
+// A function that writes the password to the password html card
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
+// Adds event listener that writes password when all functions are complete
 generateBtn.addEventListener("click", writePassword);
 
 
