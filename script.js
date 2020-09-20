087418aaf33adf77f8132d9750b086daa6a859de
Annema Cozmoz <mikewittorp@gmail.com>
2020-09-20 13:30:35 -0400

    Deleted random notes on the bottom of the javascript

--- script.js
+++ script.js
@@ -84,25 +84,3 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-
-
-// Questions ?
-
-// I don't yet know how to add "\", "]", and some other characters into array without it closing the array or making the following variables come up with an error. "[", "\", "]"
-
-// I need to figure out how to add inputted character length prompt into the password generator
-
-// Would I .push the if Statements to the password or unnecessary?
-
-// I need to put all of the possible character symbols into the password generator
-
-// I need to figure out how password generator can select the password
-
-// I need a loop that code is in that loops every time user wants to generate a new password
-
-// When do I use functions vs If Statements vs Loops vs Variables?
-
-// For my last homework I was asked to consolidate my project into more folders. Do i just create folders for images/html/etc to do so? One time I had my index.html in a folder and it displayed the ReadMe on the page. 
-
-// What is this bracket do and when is it necessary for things like .toLowerCase()
-
